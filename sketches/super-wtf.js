6f3c39a5e63e466b23c16c83e1da56bd2cabe816
Matt S Trout <mst@shadowcat.co.uk>
2024-12-29 14:33:47 +0000

    mention return values to explicate the wtfery

--- sketches/super-wtf.js
+++ sketches/super-wtf.js
@@ -8,5 +8,5 @@ const magic = { foo: derived.foo }
 
 Object.setPrototypeOf(derived, base)
 
-console.log(derived.foo())
-console.log(magic.foo())
+console.log(derived.foo()) // 3
+console.log(magic.foo())   // also 3
