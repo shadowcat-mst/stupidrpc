90f5391d82f2229d8bd9ede2d9d212a267dd0685
Matt S Trout <mst@shadowcat.co.uk>
2024-12-29 16:22:35 +0000

    this is all very SUPER::foo like and I wanted next::method. alas.

--- sketches/super-wtf.js
+++ sketches/super-wtf.js
@@ -6,7 +6,10 @@ const derived = {
 
 const magic = { foo: derived.foo }
 
+const standalone = magic.foo
+
 Object.setPrototypeOf(derived, base)
 
 console.log(derived.foo()) // 3
 console.log(magic.foo())   // also 3
+console.log(standalone())  // still 3
